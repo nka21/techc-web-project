23ee18a2d3f1fff5921a20e5b6c84820781ab5b2
nka21 <naoki.nico@gmail.com>
2024-08-12 16:10:33 +0900

    style: メインカラーとサブカラーの設定

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#0077CA",
+        secondary: "#E8F3FA",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":

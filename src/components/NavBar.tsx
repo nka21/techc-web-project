a43e4313d572a7578241d3e7a6ea2947fc29e1c7
nka21 <naoki.nico@gmail.com>
2024-08-14 18:18:41 +0900

    refactor: `NavItem`コンポーネントの作成と、各ラベルに`aria-label`の追加

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,13 @@
 import Link from "next/link";
 
+const NavItem = ({ item }: { item: string }) => (
+  <li key={item}>
+    <Link href={`#${item}`} className="hover:underline" aria-label={item}>
+      <p className="font-semibold text-2xl tracking-[8%]">{item}</p>
+    </Link>
+  </li>
+);
+
 export const NavBar = () => {
   return (
     <>
@@ -7,11 +15,7 @@ export const NavBar = () => {
         <div className="w-[260px] h-[100px] bg-white rounded-full" />
         <ul className="flex space-x-[50px]">
           {["ABOUT", "MAP", "FOOD", "RENTAL", "EVENT", "OTHER"].map((item) => (
-            <li key={item}>
-              <Link href={`#${item}`} className="hover:underline">
-                <p className="font-semibold text-2xl tracking-[8%]">{item}</p>
-              </Link>
-            </li>
+            <NavItem key={item} item={item} />
           ))}
         </ul>
       </nav>

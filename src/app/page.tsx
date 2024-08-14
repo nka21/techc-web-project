0cad768384f490efeb8f14404fa9e99cac08f788
nka21 <naoki.nico@gmail.com>
2024-08-14 16:42:18 +0900

    chore: `NavBar.tsx` コンポーネントを`src/components`に作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { NavBar } from "@/components/NavBar";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+export const NavBar = () => {
+  return (
+    <>
+      <nav>NavBar</nav>
+    </>
+  );
+};

141e702504c6a3a620126bb4e5aabc5ebe62b884
nka21 <naoki.nico@gmail.com>
2024-08-14 18:02:32 +0900

    chore: navの作成
    - focus時のスタイルは未実装

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export const NavBar = () => {
   return (
     <>
-      <nav>NavBar</nav>
+      <nav className="flex justify-between items-center">
+        <div className="w-[260px] h-[100px] bg-white rounded-full" />
+        <ul className="flex space-x-[50px]">
+          {["ABOUT", "MAP", "FOOD", "RENTAL", "EVENT", "OTHER"].map((item) => (
+            <li key={item}>
+              <Link href={`#${item}`} className="hover:underline">
+                <p className="font-semibold text-2xl tracking-[8%]">{item}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </>
   );
 };

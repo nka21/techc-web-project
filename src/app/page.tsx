ce64ba1b8dd0d64d0aa5b2717472ac844e15b6d6
nka21 <naoki.nico@gmail.com>
2024-08-22 16:16:38 +0900

    chore: aboutコンポーネントの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { About } from "./_components/About";
 import { FirstView } from "./_components/FirstView";
 
 export default function Home() {
   return (
     <div>
       <FirstView />
+      <About />
     </div>
   );
 }

af2ab02a19001d165a901f54d7c851b1bee58207
nka21 <naoki.nico@gmail.com>
2024-08-23 15:13:21 +0900

    chore: sorted class

--- src/app/_components/ArtistSchedule.tsx
+++ src/app/_components/ArtistSchedule.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ArtistSchedule = () => {
   return (
-    <section className="mt-72 px-16">
+    <section className="px-16 pt-72">
       <div className="flex flex-row items-end space-x-8">
         <h1 className="font-semibold text-6xl text-primary tracking-[6px]">
           ARTIST
@@ -12,19 +12,19 @@ const ArtistSchedule = () => {
         </h2>
       </div>
       <div className="my-12">
-        <div className="border-b-2 border-primary pb-8 space-x-4">
-          <span className="text-primary text-4xl font-semibold tracking-[7.20px]">
+        <div className="space-x-4 border-primary border-b-2 pb-8">
+          <span className="font-semibold text-4xl text-primary tracking-[7.20px]">
             午前の部
           </span>
-          <span className="text-primary text-3xl font-semibold tracking-[3px]">
+          <span className="font-semibold text-3xl text-primary tracking-[3px]">
             11:00 - 14:00
           </span>
         </div>
         <div className="space-y-4">
-          <div className="flex items-center border-b-2 border-primary py-8">
-            <div className="w-80 h-56  rounded-lg mr-6 bg-slate-300"></div>
+          <div className="flex items-center border-primary border-b-2 py-8">
+            <div className="mr-6 h-56 w-80 rounded-lg bg-slate-300" />
             <div>
-              <h5 className="text-xl font-semibold text-primary">
+              <h5 className="font-semibold text-primary text-xl">
                 SUPER BEAVER
               </h5>
               <p className="text-lg text-primary">11:00 - 11:45</p>
@@ -32,10 +32,10 @@ const ArtistSchedule = () => {
           </div>
         </div>
         <div className="space-y-4">
-          <div className="flex items-center border-b-2 border-primary py-8">
-            <div className="w-80 h-56  rounded-lg mr-6 bg-slate-300"></div>
+          <div className="flex items-center border-primary border-b-2 py-8">
+            <div className="mr-6 h-56 w-80 rounded-lg bg-slate-300" />
             <div>
-              <h5 className="text-xl font-semibold text-primary">
+              <h5 className="font-semibold text-primary text-xl">
                 SUPER BEAVER
               </h5>
               <p className="text-lg text-primary">11:00 - 11:45</p>
@@ -43,10 +43,10 @@ const ArtistSchedule = () => {
           </div>
         </div>
         <div className="space-y-4">
-          <div className="flex items-center border-b-2 border-primary py-8">
-            <div className="w-80 h-56  rounded-lg mr-6 bg-slate-300"></div>
+          <div className="flex items-center border-primary border-b-2 py-8">
+            <div className="mr-6 h-56 w-80 rounded-lg bg-slate-300" />
             <div>
-              <h5 className="text-xl font-semibold text-primary">
+              <h5 className="font-semibold text-primary text-xl">
                 SUPER BEAVER
               </h5>
               <p className="text-lg text-primary">11:00 - 11:45</p>
@@ -54,7 +54,7 @@ const ArtistSchedule = () => {
           </div>
         </div>
       </div>
-      <p className="text-sm text-gray-600 mt-8">
+      <p className="mt-8 text-gray-600 text-sm">
         ※ 物販は会場限定販売です。
         <br />
         記念品や限定グッズを多数ご用意しておりますので、この機会にぜひお求めください。

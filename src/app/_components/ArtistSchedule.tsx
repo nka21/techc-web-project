771f7aaa42fa408617cfd64654915c7b990fba6c
nka21 <naoki.nico@gmail.com>
2024-08-28 15:11:40 +0900

    style: formatの適用

--- src/app/_components/ArtistSchedule.tsx
+++ src/app/_components/ArtistSchedule.tsx
@@ -22,7 +22,12 @@ interface ScheduleSectionProps {
   items: ScheduleItemProps[];
 }
 
-const ScheduleItem: React.FC<ScheduleItemProps> = ({ name, startTime, endTime, image }) => (
+const ScheduleItem: React.FC<ScheduleItemProps> = ({
+  name,
+  startTime,
+  endTime,
+  image,
+}) => (
   <div className="flex items-center border-primary border-b-2 py-8">
     <div className="mr-6 h-56 w-80 relative rounded-lg overflow-hidden">
       <Image
@@ -33,15 +38,21 @@ const ScheduleItem: React.FC<ScheduleItemProps> = ({ name, startTime, endTime, i
       />
     </div>
     <div className="flex-1 flex flex-col justify-between h-56 py-12">
-      <h5 className="font-semibold text-primary text-4xl tracking-[7.20px]">{name}</h5>
-      <p className="text-3xl text-primary tracking-[3px]">{startTime} - {endTime}</p>
+      <h5 className="font-semibold text-primary text-4xl tracking-[7.20px]">
+        {name}
+      </h5>
+      <p className="text-3xl text-primary tracking-[3px]">
+        {startTime} - {endTime}
+      </p>
     </div>
   </div>
 );
 
-
-
-const ScheduleSection: React.FC<ScheduleSectionProps> = ({ title, timeRange, items }) => (
+const ScheduleSection: React.FC<ScheduleSectionProps> = ({
+  title,
+  timeRange,
+  items,
+}) => (
   <div className="my-12">
     <div className="space-x-4 border-primary border-b-2 py-12">
       <span className="font-semibold text-4xl text-primary tracking-[7.20px]">
@@ -67,14 +78,39 @@ const ScheduleSection: React.FC<ScheduleSectionProps> = ({ title, timeRange, ite
 
 const ArtistSchedule: React.FC = () => {
   const morningSchedule: ScheduleItemProps[] = [
-    { name: "SUPER BEAVER", startTime: "11:00", endTime: "11:45", image: superBeaver },
-    { name: "THE ORAL CIGARETTES", startTime: "11:45", endTime: "12:30", image: oralCigarettes },
-    { name: "COLD RAIN", startTime: "12:30", endTime: "13:15", image: coldRain },
+    {
+      name: "SUPER BEAVER",
+      startTime: "11:00",
+      endTime: "11:45",
+      image: superBeaver,
+    },
+    {
+      name: "THE ORAL CIGARETTES",
+      startTime: "11:45",
+      endTime: "12:30",
+      image: oralCigarettes,
+    },
+    {
+      name: "COLD RAIN",
+      startTime: "12:30",
+      endTime: "13:15",
+      image: coldRain,
+    },
   ];
 
   const afternoonSchedule: ScheduleItemProps[] = [
-    { name: "saucy dog", startTime: "15:00", endTime: "15:45", image: saucyDog },
-    { name: "HEY-SMITY", startTime: "15:45", endTime: "16:30", image: heySmity },
+    {
+      name: "saucy dog",
+      startTime: "15:00",
+      endTime: "15:45",
+      image: saucyDog,
+    },
+    {
+      name: "HEY-SMITY",
+      startTime: "15:45",
+      endTime: "16:30",
+      image: heySmity,
+    },
     { name: "ano", startTime: "16:30", endTime: "17:15", image: ano },
   ];
   return (

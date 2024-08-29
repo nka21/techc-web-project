5e7518d91890529939197a358121058266bc8562
nka21 <naoki.nico@gmail.com>
2024-08-29 19:11:56 +0900

    chore: FAQの追加

--- src/app/_components/FAQ.tsx
+++ src/app/_components/FAQ.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const FAQ = () => {
-  return <div>FAQ</div>;
+  return (
+    <section className="px-16 py-52">
+      <div className="flex items-baseline space-x-4 text-primary">
+        <h1 className="text-6xl font-semibold tracking-[6px]">FAQ</h1>
+        <span className="text-3xl font-semibold tracking-[3px]">
+          よくあるご質問
+        </span>
+      </div>
+    </section>
+  );
 };
 
 export default FAQ;

75df0ba3b1f3960dd91794b873d227643267407f
nka21 <naoki.nico@gmail.com>
2024-09-05 16:01:43 +0900

    chore: Fireworkセクションのレスポンシブ対応

--- src/app/_components/Firework.tsx
+++ src/app/_components/Firework.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 // 画像のインポート
 import firework from "@/assets/firework/firework.jpg";
 
-const Firework = () => {
+export const Firework = () => {
   return (
-    <section className="relative h-screen">
+    <section className="relative h-screen w-full">
       <Image
         src={firework}
         alt="Firework background"
@@ -15,27 +15,28 @@ const Firework = () => {
         quality={100}
         priority
       />
-      <div className="absolute inset-0 bg-black bg-opacity-30" />
-      <div className="relative z-10 p-20 h-full flex flex-col justify-between">
+      <div className="absolute inset-0 bg-black bg-opacity-70" />
+      <div className="relative container mx-auto z-10 px-4 md:px-16 py-24 h-full flex flex-col justify-between">
         <div className="flex justify-between items-center">
-          <div className="bg-white bg-opacity-90 rounded-full px-16 py-4">
-            <div className="flex items-baseline space-x-4 text-primary">
-              <h1 className="text-6xl font-semibold tracking-[6px]">
-                FIREWORK
-              </h1>
-              <span className="text-3xl font-semibold tracking-[3px]">
-                海上花火
-              </span>
-            </div>
+          <div className="items-end gap-4 flex bg-white px-10 lg:px-12 xl:px-14 py-3 lg:py-4 xl:py-5 rounded-full">
+            <h1 className="font-bold text-3xl text-primary tracking-widest lg:text-4xl xl:text-5xl">
+              FIREWORK
+            </h1>
+            <h2 className="hidden lg:block font-semibold text-primary text-xl tracking-widest md:text-2xl">
+              海上花火
+            </h2>
           </div>
-          <span className="text-white text-4xl font-semibold tracking-widest">
-            2024.8.15
-            <br />
-            20:00 - 20:30 （雨天時中止）
-          </span>
+          <div className="flex-col items-start justify-between hidden md:block gap-4">
+            <h2 className="text-white text-xl lg:text-2xl xl:text-3xl font-semibold tracking-widest">
+              2024.8.15
+            </h2>
+            <h2 className="text-white text-xl lg:text-2xl xl:text-3xl font-semibold tracking-widest">
+              20:00 - 20:30（雨天時中止）
+            </h2>
+          </div>
         </div>
         <div className="mx-auto">
-          <h2 className="text-white text-6xl font-semibold tracking-widest">
+          <h2 className="text-white text-3xl lg:text-4xl xl:text-5xl font-bold tracking-widest">
             水中花火が海上で扇状に花開く
           </h2>
         </div>
@@ -43,5 +44,3 @@ const Firework = () => {
     </section>
   );
 };
-
-export default Firework;
